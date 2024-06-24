07be045083f5f5345520bfa8c928f867af356c86
koji.sudo <sample.openmail.6@gmail.com>
2024-06-24 09:36:04 +0000

    favorite2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,6 +39,12 @@ class UsersController < ApplicationController
     counts(@user)
   end
   
+  def likes
+    @user = User.find(params[:id])
+    @pagy, @likes = pagy(@user.favorite_posts)
+    counts(@user)
+  end
+  
   private
   
   def user_params
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,8 @@ class User < ApplicationRecord
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
+  has_many :favorites
+  has_many :favorite_posts, through: :favorites, source: :micropost
 
   def follow(other_user)
     unless self == other_user
@@ -31,4 +33,16 @@ class User < ApplicationRecord
     Micropost.where(user_id: self.following_ids + [self.id])
   end
   
+  def add_favorite(other_micropost)
+    self.favorites.find_or_create_by(micropost_id: other_micropost.id)
+  end
+  
+  def delete_favorite(other_micropost)
+    favorite = self.favorites.find_by(micropost_id: other_micropost.id)
+    favorite.destroy if favorite
+  end
+  
+  def favorite?(other_micropost)
+    self.favorites.exists?(micropost_id: other_micropost.id)
+  end
 end
